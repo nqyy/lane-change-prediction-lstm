d76f417e610d81a682327739cb034c09f4f40f8d
qgao10 <qgao10@illinois.edu>
2019-11-30 18:13:50 -0600

    3 lstm layer

--- rnn_model.py
+++ rnn_model.py
@@ -5,13 +5,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import time
 
 torch.manual_seed(1)
 
 
 #load data into pytorch tensor
 input_data = []
-for i in range(1, 11):
+for i in range(1, 57):
     idx_str = '{0:02}'.format(i)
     pickle_in = open("output/result"+idx_str+".pickle","rb")
     temp_data = pickle.load(pickle_in)
@@ -20,21 +21,22 @@ for i in range(1, 11):
 
 # pickle_in = open("result.pickle","rb")
 # input_data = pickle.load(pickle_in)
-random.shuffle(input_data)
+# random.shuffle(input_data)
 print(len(input_data))
 size = len(input_data)
 training_set = input_data[:int(size*0.8)]
-print(len(training_set[0]))
 testing_set = input_data[int(size*0.8):]
 
+#set hyper-parameters
 state_dim = 16
 states_per_sequence = 25
 hidden_dim = 256
-output_size = 2
+output_size = 3
 num_epoch = 10
+num_lstm_layers = 3
 
 class PredicterRNN(nn.Module):
-    def __init__(self, input_dim=state_dim*states_per_sequence, hidden_dim=256, output_size=2, batch_size=states_per_sequence, num_layers=1):
+    def __init__(self, input_dim, hidden_dim, output_size, batch_size, num_layers):
         super(PredicterRNN, self).__init__()
         # The LSTM takes word embeddings as inputs, and outputs hidden states
         # with dimensionality hidden_dim.
@@ -42,7 +44,7 @@ class PredicterRNN(nn.Module):
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
         self.batch_size = batch_size
-
+        self.dropout_layer = nn.Dropout(p=0.2)
         self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers)
 
         # The linear layer that maps from hidden state space to output space
@@ -58,16 +60,20 @@ class PredicterRNN(nn.Module):
         input = input.view(-1, 25, 16)
         #print(input.size())
         lstm_out, _ = self.lstm(input)#.view(state_dim, self.batch_size, -1))
+
         output_space = self.dense1(lstm_out.view(self.batch_size, -1))
         output_space = torch.tanh(output_space)
+        #output_space = self.dropout_layer(output_space)
         output_space = self.dense2(output_space)
         output_space = torch.tanh(output_space)
         output_scores = F.log_softmax(output_space, dim=1)
         return output_scores
 
 
-model = PredicterRNN(state_dim, hidden_dim, output_size, states_per_sequence)
+model = PredicterRNN(state_dim, hidden_dim, output_size, states_per_sequence, num_lstm_layers)
 loss_function = nn.NLLLoss()
+#loss_function = nn.MSELoss()
+#optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
 optimizer = optim.SGD(model.parameters(), lr=0.1)
 
 
@@ -83,6 +89,7 @@ def prepare_sequence(state_sequence, tag):
 
 for epoch in range(num_epoch):
     random.shuffle(training_set)
+    start = time.time()
     for state_sequence, tag in training_set:
         # Step 1. Remember that Pytorch accumulates gradients.
         # We need to clear them out before each instance
@@ -101,13 +108,16 @@ for epoch in range(num_epoch):
         loss.backward()
         optimizer.step()
 
+    end = time.time()
+    print("Epoch", epoch, " run time:", end-start)
+
     # # See what the scores are after training
     correct, total = 0, 0
     with torch.no_grad():
         for state_sequence, tag in testing_set:
             sentence_in, label = prepare_sequence(state_sequence, tag)
             output_scores = model(sentence_in)
-            val, idx = output_scores[-1].max(0)
+            _, idx = output_scores[-1].max(0)
             if idx == tag:
                 correct += 1
             total += 1
